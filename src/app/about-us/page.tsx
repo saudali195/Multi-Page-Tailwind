e17e0f6530c58b62fb0fd9fcb0a6fd7e4f412c19
saudali195 <160694354+saudali195@users.noreply.github.com>
2024-11-11 00:15:51 +0500

    Add files via upload

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100 py-10">
+        <div className="w-full max-w-2xl bg-white rounded-lg shadow-lg p-6 space-y-6 text-center">
+          <h2 className="text-2xl font-bold">About Me</h2>
+          <div className="space-y-4">
+            <p className="text-lg ">My name is Saud Muhammad Ali I am a Full Stack Developer.</p>
+            <p className="text-base">
+              As a frontend developer, I specialize in crafting responsive, user-friendly, and visually engaging interfaces. With a strong foundation in HTML, CSS, and JavaScript, I am skilled in translating design concepts into functional, interactive web experiences. My focus is on writing clean, efficient code that enhances both performance and user satisfaction, and I continuously strive to stay updated with the latest technologies to create seamless and accessible applications.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
